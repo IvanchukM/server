a4d8e71bc1f30bd6418c466a9dd75621a7032105
Ivanchuk Mikita <mikitaivanchuk@gmail.com>
2020-05-05 19:17:43 +0300

    minor changes

--- coffee.js
+++ coffee.js
@@ -9,8 +9,8 @@ module.exports = [
     {
         name: "americano",
         volumeMedium: "250",
-        volumeBig: "2.20",
-        priceMedium: "350",
+        volumeBig: "350",
+        priceMedium: "2.20",
         priceBig: "2.70"
     }
 ]
