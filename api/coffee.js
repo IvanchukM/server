565d69f697268d0a5324965bba033b8b4929b194
Ivanchuk Mikita <mikitaivanchuk@gmail.com>
2020-05-19 14:33:34 +0300

    +

--- api/coffee.js
+++ api/coffee.js
@@ -42,13 +42,13 @@ router.get('/:id', isValidId, (req,res,next) => {
    });
 });
 
-router.post('/', (req,res,next) =>{
+router.post('/', (req,res,next,err) =>{
     if(validCoffee(req.body)){
        queries.create(req.body).then(coffee =>{
            res.json(coffee);
        });
     }else{
-        next(new Error('Invalid data'));
+        next(new Error(err));
     }
 });
 
