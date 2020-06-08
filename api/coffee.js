6f29f33136b51c385b21bea6661586ea15161214
Ivanchuk Mikita <43702030+IvanchukM@users.noreply.github.com>
2020-06-08 13:49:57 +0300

    Update coffee.js

--- api/coffee.js
+++ api/coffee.js
@@ -8,12 +8,6 @@ function isValidId(req,res,next){
     if(!isNaN(req.params.id)) return next();
     next(new Error('Invalid ID'));
 }
-function validCoffee(coffee){
-    const hasName = typeof coffee.name == 'string' && coffee.name.trim() != '';
-    const hasVolume = typeof coffee.volume == 'string' && coffee.volume.trim() != '';
-    const hasPrice = typeof coffee.price == 'string' && coffee.price.trim() != '';
-    return hasName && hasVolume && hasPrice
-}
 router.get('/', (req,res) => {
     queries.getAll().then(coffee =>{
         res.json(coffee);
@@ -43,24 +37,16 @@ router.get('/:id', isValidId, (req,res,next) => {
 });
 
 router.post('/', (req,res,err) =>{
-    if(validCoffee(req.body)){
        queries.create(req.body).then(coffee =>{
            res.json(coffee);
        });
-    }else{
-        next(new Error(err));
-    }
 });
 
 router.put('/:id', isValidId, (req,res,next) => {
-    if(validCoffee(req.body)){
         //update coffe
         queries.update(req.params.id, req.body).then(coffee =>{
             res.json(coffee);
         });
-    }else{
-        next(new Error('Invalid data'));
-    }
 });
 
 router.delete('/:id', isValidId, (req,res) => {
